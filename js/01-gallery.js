f9c36cc80bb829aeeb164b5f00818780bbf631c1
Frost15777 <di.yu.mk@gmail.com>
2023-10-29 10:27:15 +0100

    main

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems.map(({ preview, original, description }) =>
+    `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}"
+    data-source="${original}"
+    alt="${description}"
+    />
+  </a>
+</li>`).join('');
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+gallery.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target.tagName === 'IMG') {
+        const instance = basicLightbox.create(
+            `<img src="${event.target.dataset.source}" alt="${event.target.alt}">`);
+
+        instance.show()
+
+    }
+});
+
 console.log(galleryItems);
